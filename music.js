ea83c05c0ecd49e7ff498d17055a7c32e1b412d7
Alex Tsang <alextsang@live.com>
2021-08-30 11:15:54 +0900

    PLS generation script

---
+++ music.js
@@ -0,0 +1,48 @@
+/**
+ * Generate a music playlist (PLS) from a directory of music files.
+ *
+ * Author: Alex TSANG <alextsang@live.com>
+ * License: The 3-Clause BSD License
+ */
+
+const fs = require('fs');
+
+const SCAN_DIRECTORY = 'C:\\Users\\alext\\Music';
+const IGNORED_FILES = [
+  'desktop.ini',
+  'Instrumental'
+];
+const SERVER = 'https://www.example.net';
+const SERVER_MUSIC_DIRECTORY = 'music';
+const OUTPUT_FILE = 'C:\\Users\\alext\\Downloads\\music.pls';
+
+const fsPromises = fs.promises;
+
+/**
+ * Generates the playlist.
+ *
+ * @param {string[]} files Filenames.
+ *
+ * @returns {string} Music playlist in PLS format.
+ */
+const generatePLS = (files) => {
+  const lines = [];
+  lines.push('[playlist]');
+  files.forEach((file, index) => {
+    const url = new URL(`${SERVER_MUSIC_DIRECTORY}/${file}`, SERVER);
+    lines.push(`File${(index + 1)}=${url.href}`);
+    lines.push(`Title${(index + 1)}=${file}`);
+  });
+  lines.push(`NumberOfEntries=${files.length}`);
+  lines.push('Version=2');
+  return lines.join('\n');
+};
+
+const main = async () => {
+  const files = await fsPromises.readdir(SCAN_DIRECTORY);
+  const musicFiles = files.filter((file) => !IGNORED_FILES.includes(file));
+  const content = generatePLS(musicFiles);
+  await fsPromises.writeFile(OUTPUT_FILE, content);
+};
+
+main();
